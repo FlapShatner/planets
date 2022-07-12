6addc5573b89b194122edf16baa6ea177b02677d
Jordan Roberts <jrobertswebdev@gmail.com>
2022-07-12 01:01:35 -0500

    adjust stat boxes media query to 1000

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "31bdc4b970777ca3"; }
+/******/ 		__webpack_require__.h = function() { return "44a4c6f45c46f7c8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
