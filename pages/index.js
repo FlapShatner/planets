9b30b9ff2610fdbdf42c4453f74e570724560e19
Jordan Roberts <jrobertswebdev@gmail.com>
2022-07-12 00:34:42 -0500

    ignore .next

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,1 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
 
-export async function getStaticProps() {
-  return {
-    redirect: {
-      destination: '/planets/mercury',
-      permanent: true,
-    },
-  }
-}
