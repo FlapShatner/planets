f55fa1bbf7dd401e11b4c069cee4c7c8fd4f7368
Jordan Roberts <jrobertswebdev@gmail.com>
2022-07-12 00:38:15 -0500

    change index redirect

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "84f25c5ffd2e74ac"; }
+/******/ 		__webpack_require__.h = function() { return "9a6bcb9a1079e985"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
